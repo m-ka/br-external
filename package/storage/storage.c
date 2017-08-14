e581f22d16261acf40ee6bd234f5b63f0cb779bc
Mikhail Karpenko <karpenko_m@mail.ru>
2017-08-14 21:11:16 +0600

    Implement file operation functions in driver

--- package/storage/storage.c
+++ package/storage/storage.c
@@ -3,7 +3,10 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
+#include <linux/uaccess.h>
+#include <linux/kdev_t.h>
 
+#define DEV_MAJOR                 251
 #define DRV_NAME                  "barx-storage"
 #define SYSFS_DIR_NAME            "barx"
 #define OF_PROP_NAME              "size"
@@ -18,7 +21,10 @@ struct driver_data {
 
 	dev_t dev_num;
 	struct cdev cdev;
+	int device_created;
 };
+
+static void clean_up(struct device *dev, struct driver_data *d_data);
 
 static unsigned int of_get_size(struct device *dev)
 {
@@ -53,27 +59,28 @@ static int init_storage(struct platform_device *pdev)
 	d_data->size = of_get_size(dev);
 	if (d_data->size == 0) {
 		dev_err(dev, "got inappropriate storage size from device tree\n");
-		devm_kfree(dev, d_data);
+		clean_up(dev, d_data);
 		return -EINVAL;
 	}
 	d_data->data = devm_kzalloc(dev, d_data->size, GFP_KERNEL);
 	if (!d_data->data) {
 		dev_err(dev, "could not allocate memory for data storage\n");
-		devm_kfree(dev, d_data);
+		clean_up(dev, d_data);
 		return -ENOMEM;
 	}
 
-	ret = alloc_chrdev_region(&d_data->dev_num, 0, 1, DRV_NAME);
-	if (ret) {
+	d_data->dev_num = MKDEV(DEV_MAJOR, 0);
+	ret = register_chrdev_region(d_data->dev_num, 1, DRV_NAME);
+	if (ret < 0) {
 		dev_err(dev, "could not allocate device number\n");
-		devm_kfree(dev, d_data);
+		clean_up(dev, d_data);
 		return ret;
 	}
 
 	ret = sysfs_create_group(&dev->kobj, &dev_attr_root_group);
 	if (ret) {
 		dev_err(dev, "sysfs group creation failed\n");
-		devm_kfree(dev, d_data);
+		clean_up(dev, d_data);
 		return ret;
 	}
 
@@ -82,10 +89,11 @@ static int init_storage(struct platform_device *pdev)
 	ret = cdev_add(&d_data->cdev, d_data->dev_num, 1);
 	if (ret) {
 		dev_err(dev, "Could not add character device\n");
-		devm_kfree(dev, d_data);
+		clean_up(dev, d_data);
 		return ret;
 	}
 	dev_set_drvdata(dev, d_data);
+	d_data->device_created = 1;
 
 	return 0;
 }
@@ -104,45 +112,107 @@ static int remove_storage(struct platform_device *pdev)
 	return 0;
 }
 
+static void clean_up(struct device *dev, struct driver_data *d_data)
+{
+	if (d_data->device_created) {
+		cdev_del(&d_data->cdev);
+	}
+	if (MAJOR(d_data->dev_num))
+		unregister_chrdev_region(d_data->dev_num, 1);
+	if (d_data->data)
+		devm_kfree(dev, d_data->data);
+	if (d_data)
+		devm_kfree(dev, d_data);
+}
+
 static int storage_open(struct inode *inode, struct file *file)
 {
+	struct driver_data *d_data = container_of(inode->i_cdev, struct driver_data, cdev);
+	file->private_data = d_data;
+
 	return 0;
 }
 
 static loff_t storage_lseek(struct file *file, loff_t offset, int orig)
 {
+	loff_t new_pos = 0;
+	struct driver_data *d_data = file->private_data;
 
+	switch (orig) {
+		case SEEK_SET:
+			new_pos = offset;
+			break;
+		case SEEK_CUR:
+			new_pos = file->f_pos + offset;
+			break;
+		case SEEK_END:
+			new_pos = d_data->size - offset;
+			break;
+	}
+	if ((new_pos >= 0) && (new_pos < d_data->size))
+		file->f_pos = new_pos;
+	else
+		new_pos = -EINVAL;
+
+	return new_pos;
 }
 
 static ssize_t storage_read(struct file *file, char *buff, size_t count, loff_t *off)
 {
+	ssize_t ret_val = 0;
+	struct driver_data *d_data = file->private_data;
 
+	if (*off >= d_data->size)
+		return ret_val;
+	if (*off + count > d_data->size)
+		count = d_data->size - *off;
+
+	if (copy_to_user(buff, &d_data->data[*off], count))
+		return -EFAULT;
+	*off += count;
+	d_data->read_cntr++;
+	ret_val = count;
+
+	return ret_val;
 }
 
 static ssize_t storage_write(struct file *file, const char *buff, size_t count, loff_t *off)
 {
+	ssize_t ret_val = 0;
+	struct driver_data *d_data = file->private_data;
 
+	if (*off >= d_data->size)
+		return ret_val;
+	if (*off + count > d_data->size)
+		count = d_data->size - *off;
+
+	if (copy_from_user(&d_data->data[*off], buff, count))
+		return -EFAULT;
+	*off += count;
+	d_data->write_cntr++;
+	ret_val = count;
+
+	return ret_val;
 }
 
 static int storage_release(struct inode *inode, struct file *file)
 {
+	/* nothing to be done here */
 	return 0;
 }
 
 static ssize_t read_cntr_show(struct device *dev, struct device_attribute *attr, char *buff)
 {
-	int len;
 	struct driver_data *d_data = dev_get_drvdata(dev);
 
-	return len;
+	return sprintf(buff, "%u\n", d_data->read_cntr);
 }
 
 static ssize_t write_cntr_show(struct device *dev, struct device_attribute *attr, char *buff)
 {
-	int len;
 	struct driver_data *d_data = dev_get_drvdata(dev);
 
-	return len;
+	return sprintf(buff, "%u\n", d_data->write_cntr);
 }
 
 static struct file_operations driver_fops = {
